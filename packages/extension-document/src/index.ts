00f0a413ebb2f9b7d57d6256a16010e35e07633c
Yatharth K <yatharth01@gmail.com>
2024-01-08 08:23:29 +0530

    extension-document: read version from package

--- packages/extension-document/src/index.ts
+++ packages/extension-document/src/index.ts
@@ -1,8 +1,9 @@
 import { Document as TiptapDocument } from "@tiptap/extension-document";
+import pkg from "../package.json";
 
 export * from "@tiptap/extension-document";
 
-export const VERSION = "0.0.1";
+export const VERSION = pkg.version;
 
 export const Document = TiptapDocument.extend({
   addAttributes() {
