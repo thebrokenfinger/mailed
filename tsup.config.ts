87373f77725d032eb9279d7185cf3f3ffcfb2c05
Yatharth K <yatharth01@gmail.com>
2024-02-06 21:33:52 +0530

    replace microbundle with tsup

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => {
+  return {
+    entry: options.entry,
+    outDir: options.outDir,
+    dts: true,
+    splitting: true,
+    clean: true,
+    format: ["esm", "cjs"],
+  };
+});
